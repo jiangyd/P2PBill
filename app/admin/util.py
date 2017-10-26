f7a8ac8116045fccd37852399b7f8fa60013f61b
江永道 <jiangyd@jiangyd.local>
2017-10-26 18:45:21 +0800

    阿里签名优化

--- app/admin/util.py
+++ app/admin/util.py
@@ -4,8 +4,10 @@ import hmac
 from hashlib import sha1
 import base64
 from datetime import datetime
-# import urllib
 import requests
+import urllib.parse
+from app import app
+
 
 
 def get_secret():
@@ -13,52 +15,77 @@ def get_secret():
     return data
 
 
-def replacestr(str):
-    data = str.replace("+", "%20")
-    data = data.replace("*", "%2A")
-    data = data.replace("%7E", '~')
-    return data
+
+class SendMailByAli(object):
+    def __init__(self):
+        self.access_keyid=app.config["access_keyid"]
+        self.access_secret=app.config["access_secret"]
+
+    def __replace(self,str):
+        data = str.replace("+", "%20")
+        data = data.replace("*", "%2A")
+        data = data.replace("%7E", '~')
+        return data
+    def __encry(self,secret,data):
+        hash_value=hmac.new((secret + "&").encode("utf-8"),
+                            data.encode("utf-8"), sha1).digest().strip()
+        return base64.b64encode(hash_value).decode("utf-8")
+
+    def __utcnow(self):
+        return datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    def __nonce(self):
+        """获取数据uuid"""
+        return str(uuid.uuid4())
+    def params(self,subject,toaddress,htmlbody="",textbody=""):
+        utctime=self.__utcnow()
+        Nonce=self.__nonce()
+        params = {"Action": "SingleSendMail",
+                  "AccountName": "admin@testwd.cn",
+                  "ReplyToAddress": "true",
+                  "AddressType": "1",
+                  "ToAddress": toaddress,
+                  "FromAlias": "P2PBill",
+                  "Subject": subject,
+                  "Format": "JSON",
+                  "AccessKeyId":self.access_keyid ,
+                  "Timestamp": utctime,
+                  "SignatureMethod": "HMAC-SHA1",
+                  "SignatureVersion": "1.0",
+                  "SignatureNonce": Nonce,
+                  "Version": "2015-11-23", }
+        if htmlbody:
+            print("gg")
+            params["HtmlBody"]=htmlbody
+        if textbody:
+            print("Cc")
+            params["TextBody"]=textbody
+        print(params)
+        sort_params = {}
+        for key in sorted(params):
+            sort_params[key] = params[key]
+        return sort_params
+    def send_mail(self,subject,toaddress,htmlbody=None,textbody=None):
+        params_data=self.params(subject,toaddress,htmlbody=None,textbody=None)
+        data=urllib.parse.urlencode(params_data)
+        StringToSign="GET&%2F&" + self.__replace(requests.utils.quote(data,safe=""))
+        Signature=self.__encry(self.access_secret,StringToSign)
+        params_data["Signature"]=Signature
+        url="https://dm.aliyuncs.com/?" +urllib.parse.urlencode(params_data)
+        res=requests.get(url)
+        return {"code":res.status_code,"msg":res.text}
 
 
-def hash_value(secret, data):
-    hashdata = hmac.new((secret + "&").encode("utf-8"), data.encode("utf-8"), sha1).digest().strip()
-    return base64.b64encode(hashdata)
 
 
-def get_utc_now():
-    return datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
-def get_params(subject, emailaddress, accesskeyid, HtmlBody=None, TextBody=None):
-    utctime = get_utc_now()
-    nonce = uuid.uuid4()
-    params = {"Action": "SingleSendMail",
-              "AccountName": "admin@testwd.cn",
-              "ReplyToAddress": True,
-              "AddressType": 1,
-              "ToAddress": emailaddress,
-              "FromAlias": "P2PBill",
-              "Subject": subject,
-              "Format": "JSON",
-              "Version": "2015-11-23",
-              "AccessKeyId": accesskeyid,
-              "Timestamp": utctime,
-              "SignatureMethod": "HMAC-SHA1",
-              "SignatureVersion": "1.0",
-              "SignatureNonce": nonce}
-    return ''.join([k + "=" + str(params[k]) + "&" for k in sorted(params)])
+
+    # return '&'.join([k + "=" + str(params[k])  for k in sorted(params)])
 
 
-paran = get_params("测试", "jiangyd@jiagouyun.com", "LTAIVF29fBCXjmYC", TextBody="adsfdf")
-print(paran)
-data = replacestr(paran)
-endata = requests.utils.quote(data)
-print(endata)
-StringToSign = "GET" + "&" + requests.utils.quote("/", safe="") + "&" + endata
 
-v = hash_value("i9IkzpKWCuSwK808iNR6awWREjsvU4", StringToSign)
-print(v)
-url = "https://dm.aliyuncs.com/?" + data + "Signature=" + v.decode("utf-8")
-print(url)
-res = requests.get(url)
-print(res.status_code, res.text)
+
+a=SendMailByAli()
+print(a.send_mail("测试","962584902@qq.com",textbody="test"))
+
