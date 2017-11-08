079b265738ed614ef65ee6727e692d4aebf4c832
jiangyd <962584902@qq.com>
2017-11-08 08:07:03 +0800

    扩展启动方式更改

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,34 @@
 from flask import Flask,render_template
+from app.crontab import JOBS
+
+from app.ext import db,scheduler
 
 import os
 
-from flask_sqlalchemy import SQLAlchemy
+
+
 
 app=Flask(__name__)
+
 app.config["SECRET_KEY" ]='you-will-never-guess'
-app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://root:123456@192.168.56.102:3377/p2pbill"
+app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://root:123456@127.0.0.1:3306/p2pbill"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=True
+app.config["SQLALCHEMY_ECHO"]=True
+
+db.app=app
+db.init_app(app)
 
 #头像保存位置
 UPLOAD_FOLDER=os.path.join(os.path.dirname(os.path.abspath(__file__)),"static/uploads")
 app.config['UPLOAD_FOLDER'] =UPLOAD_FOLDER
-db=SQLAlchemy(app)
+
+
+
+
+app.config["JOBS"]=JOBS
+
+scheduler.init_app(app)
+scheduler.start()
 
 from app.admin import admin as admin_blueprint
 app.register_blueprint(admin_blueprint,url_prefix="/admin")
