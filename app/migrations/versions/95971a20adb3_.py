8588a7c5e74bd704e9ca5bbf39739cde169c9dd3
江永道 <jiangyd@jiangyd.local>
2017-11-27 09:52:10 +0800

    清理pyc文件

---
+++ app/migrations/versions/95971a20adb3_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 95971a20adb3
+Revises: ccaed15190ca
+Create Date: 2017-10-27 16:47:19.252124
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '95971a20adb3'
+down_revision = 'ccaed15190ca'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
